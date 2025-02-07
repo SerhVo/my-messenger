4de6c3de17c551162c57e7eda1d630466a37bc41
Serhii Vorotnoi <svorotnoy@gmail.com>
2025-02-07 17:06:38 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,30 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   server: {
-    host: true, 
+    https: {
+      key: fs.readFileSync("./certs/localhost-key.pem"),
+      cert: fs.readFileSync("./certs/localhost.pem"),
+    },
+    host: "192.168.1.108",
+    port: 5173,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Отдельный чанк для зависимостей
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Поднять лимит, если нужно
   },
 });
